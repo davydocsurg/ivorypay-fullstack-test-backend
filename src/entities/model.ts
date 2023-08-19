3a8d6e530d6c7fe29d06d02ccb62b9ab69d5e48d
davydocsurg <davydocsurg@gmail.com>
2023-08-19 03:38:23 +0100

    update database entities

--- src/entities/model.ts
+++ src/entities/model.ts
@@ -6,7 +6,7 @@ import {
 } from "typeorm";
 
 export default abstract class Model extends BaseEntity {
-    @PrimaryGeneratedColumn("id")
+    @PrimaryGeneratedColumn()
     id: string;
 
     @CreateDateColumn()
