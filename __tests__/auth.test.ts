7fe56c9b16ef2315c82ae797ec02a3a71b008f14
davydocsurg <davydocsurg@gmail.com>
2023-08-23 04:53:16 +0100

    update all entities to include tsts, add login test #11

--- __tests__/auth.test.ts
+++ __tests__/auth.test.ts
@@ -2,7 +2,7 @@ import { userData } from "./__mocks__/data";
 import httpStatus from "http-status";
 import { describe, test, expect } from "@jest/globals";
 import setUpDB from "./utils/setUpDB";
-import { fetchAdmin, registerUser } from "./shared/commands";
+import { fetchAdmin, loginUser, registerUser } from "./shared/commands";
 
 setUpDB();
 
@@ -16,5 +16,21 @@ describe("Auth routes", () => {
 
         expect(res.status).toBe(httpStatus.CREATED);
         expect(res.body).toEqual({ ...res.body });
+    });
+
+    test("should return 201 and successfully register user if request data is ok", async () => {
+        const adminRes = await fetchAdmin();
+        const newUserRes = await registerUser(
+            userData,
+            adminRes.body.admin.referralCode
+        );
+        const { email, password } = newUserRes.body;
+
+        const res = await loginUser({ email, password });
+
+        expect(res.status).toBe(httpStatus.OK);
+        expect(res.body).toEqual({
+            ...res.body,
+        });
     });
 });
