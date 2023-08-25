003a117b534314b65a8ac3b7d41b46b0cef3a914
davydocsurg <davydocsurg@gmail.com>
2023-08-26 00:39:32 +0100

    update cors setup

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request } from "express";
 import httpStatus from "http-status";
 import cors from "cors";
 import { config, errorHandler, successHandler } from "./config";
@@ -7,15 +7,36 @@ import routes from "./routes";
 import { ApiError } from "./utils";
 
 const app = express();
+const allowlist = [
+    "https://ivorypay-fullstack-test-frontend.vercel.app/",
+    "http://localhost:3000",
+    "http://localhost:5173",
+];
+// process.env.FRONT_END_URL,
+const corsOptionsDelegate = function (
+    req: Request,
+    callback: (err: any, corsOptions: any) => void
+) {
+    let corsOptions: any;
+    if (allowlist.indexOf(req.header("Origin")!) !== -1) {
+        corsOptions = {
+            origin: true,
+            methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+            credentials: true,
+            exposedHeaders: ["Bearer Token"],
+        }; // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions);
+};
+
+app.use(cors(corsOptionsDelegate));
 
 if (config.env !== "test") {
     app.use(successHandler);
     app.use(errorHandler);
 }
-
-// enable cors
-app.use(cors());
-app.options("*", cors());
 
 // parse json request body
 app.use(express.json());
