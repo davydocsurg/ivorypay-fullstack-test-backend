056ef9ea3b79602b5c2ed4d76d74d19ce44d50e7
davydocsurg <davydocsurg@gmail.com>
2023-08-25 16:47:49 +0100

    update data source

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -16,6 +16,7 @@ export const AppDataSource = new DataSource({
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     synchronize: false,
+    ssl: true,
     logging: false,
     entities: entities,
     migrations: migrations,
