4332e4df30e9e45e7644da024059ff078e5a8bec
davydocsurg <davydocsurg@gmail.com>
2023-08-25 07:24:13 +0100

    setup API docs with swagger - add auth docs #11

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,7 +6,7 @@ import { authValidation } from "../validations";
 const authRoute = express.Router();
 
 authRoute.post(
-    "/register", // optionally include role
+    "/register",
     validate(authValidation.register),
     authController.register
 );
@@ -14,3 +14,142 @@ authRoute.post("/login", validate(authValidation.login), authController.login);
 authRoute.post("/logout", isAuthenticated, authController.logout);
 
 export default authRoute;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentication
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register as user
+ *     tags: [Auth]
+ *     parameters:
+ *       - in: query
+ *         name: referralCode
+ *         schema:
+ *           type: string
+ *         description: Referral code (required)
+ *       - in: query
+ *         name: role
+ *         schema:
+ *           type: string
+ *         description: Role of the user (e.g., 'admin', 'user')
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *              - firstName
+ *              - lastName
+ *              - email
+ *              - password
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: must be unique
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 minLength: 8
+ *                 description: At least one number and one capital letter and one special character
+ *             example:
+ *               firstName: fake first name
+ *               lastName: fake last name
+ *               email: fake@example.com
+ *               password: Password1@
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   $ref: '#/database/entities/User'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *                 format: password
+ *             example:
+ *               email: fake@example.com
+ *               password: Password1!
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   $ref: '#/database/entities/User'
+ *       "401":
+ *         description: Invalid email or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/database/entities/Error'
+ *             example:
+ *               code: 401
+ *               message: Invalid email or password
+ */
+
+/**
+ * @swagger
+ * /auth/logout:
+ *   post:
+ *     summary: Logout
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: Logged out successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Logged out successfully
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerError'
+ */
